c2495046a0f07895bb50c5c4790f1ff446593053
Reese Sorrell <joreesesorrell@gmail.com>
2022-12-13 10:25:11 -0600

    Add function to populate grid display
    Create function "create board" inside display controller module which
    will read the game board array and create on screen buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gameBoard = (() => {
     //create original empty game board
-    let boardArray = [[null, null, null],[null, null, null],[null, null, null]]
+    let boardArray = [[null, 'X', null, null],[null, null, null],[null, null, null]]
 
     //return current board state
     const getBoard = () => boardArray;
@@ -10,5 +10,21 @@ const gameBoard = (() => {
 })();
 
 const displayController = (() => {
-    
+    const gridArea = document.querySelector('.grid')
+
+    //create 9 buttons in order of the board array with the content of the 3x3 array
+    const createBoard = (boardArray) => {
+        for (let row=0; row<=2; row++) {
+            for (let column=0; column<=2; column++) {
+                const newSpace = document.createElement('button');
+                if (boardArray[row][column]) {
+                    newSpace.textContent = boardArray[row][column];
+                }
+                gridArea.appendChild(newSpace);
+            }
+        }
+    }
+    return {createBoard}
 })();
+
+displayController.createBoard(gameBoard.getBoard())
