3a65b190b4e032f264d274dd414388d96b9d6f82
Reese Sorrell <joreesesorrell@gmail.com>
2022-12-14 17:35:45 -0600

    Add player constructor and space class adder

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const gameBoard = (() => {
     //create original empty game board
-    let boardArray = [[null, 'X', null, null],[null, null, null],[null, null, null]]
+    let boardArray = [[null, null, null, null],[null, null, null],[null, null, null]]
 
     //return current board state
     const getBoard = () => boardArray;
@@ -12,19 +12,50 @@ const gameBoard = (() => {
 const displayController = (() => {
     const gridArea = document.querySelector('.grid')
 
+    let turnCounter = 0;
+
+    //decides whose turn it is based off the turn counter
+    const turnDecider = () => {
+        if (turnCounter%2) {
+            turnCounter++;
+            return player2;
+        }
+        else {
+            turnCounter++;
+            return player1;
+        }
+    }
+
     //create 9 buttons in order of the board array with the content of the 3x3 array
-    const createBoard = (boardArray) => {
+    const updateBoard = (boardArray) => {
         for (let row=0; row<=2; row++) {
             for (let column=0; column<=2; column++) {
+                //make buttons with rows and columns as their class
                 const newSpace = document.createElement('button');
+                newSpace.classList.add(row + ',' + column);
+
+                newSpace.onclick = turnDecider();
+
+                //set DOM content to the board array content
                 if (boardArray[row][column]) {
                     newSpace.textContent = boardArray[row][column];
                 }
+
                 gridArea.appendChild(newSpace);
             }
         }
     }
-    return {createBoard}
+    return {updateBoard}
 })();
 
-displayController.createBoard(gameBoard.getBoard())
+const playerCreater = (name, sign) => {
+    clickSpace = () => {
+        console.log(name)
+    }
+    return {name, sign, clickSpace};
+}
+
+const player1 = playerCreater('Reese', 'X');
+const player2 = playerCreater('Kacy', 'O');
+
+displayController.updateBoard(gameBoard.getBoard())
