107ef7f3ed818fdb1823fd22ae9a77dbd0df7de8
Reese Sorrell <joreesesorrell@gmail.com>
2022-12-15 10:38:59 -0600

    Add checkWin function that checks for a winner after every move

--- script.js
+++ script.js
@@ -7,31 +7,61 @@ const gameBoard = (() => {
 
     let turnCounter = 0;
 
-    //decides whose turn it is based off the turn counter
-    const turnDecider = (button) => {
+    //calls decide turn to get the mark of the player who's turn it is
+    //finds the x and y of the button and calls the mark space on it
+    const makeMove = (button) => {
+        let mark = decideTurn().sign;
+        let x = button.classList[0][0];
+        let y = button.classList[0][2];
+        markSpace(x, y, mark);
+    }
+
+    //decides who's turn it is by dividing turn counter by 2
+    const decideTurn = () => {
         if (turnCounter%2) {
-            player2.clickSpace(button);
+            return player2;
         }
         else {
-            player1.clickSpace(button);
+            return player1;
         }
     }
 
-    const makeMove = (x, y, sign) => {
+    //makes a move if a space is empty given an x, y, and sign to fill the space with
+    const markSpace = (x, y, sign) => {
         if (!boardArray[x][y]) {
             boardArray[x][y] = sign;
-            turnCounter++;
             checkWin();
+            turnCounter++;
             displayController.updateBoard(getBoard());
         }
     }
 
+    //checks to see if a player has won
     const checkWin = () => {
-        console.log('working');
+        for (let i=0; i<=2; i++) {
+            if (boardArray[i][0]) {
+                if (boardArray[i][0] == boardArray[i][1] && boardArray[i][0] == boardArray[i][2]) {
+                    console.log('WINNER');
+                }
+            }
+            if (boardArray[0][i]) {
+                if (boardArray[0][i] == boardArray[1][i] && boardArray[0][i] == boardArray[2][i]) {
+                    console.log('WINNER');
+                }
+            }
+        }
+        if (boardArray[1][1]) {
+            if (boardArray[0][0] == boardArray[1][1] && boardArray[0][0] == boardArray[2][2]) {
+                console.log('WINNER');
+            }
+            if (boardArray[0][2] == boardArray[1][1] && boardArray[0][2] == boardArray[2][0]) {
+                console.log('WINNER');
+            }
+        }
     }
 
     //return all public module functions and objects
-    return {getBoard, turnDecider, makeMove};
+    return {getBoard, makeMove};
 })();
 
 const displayController = (() => {
@@ -46,7 +76,7 @@ const displayController = (() => {
                 const newSpace = document.createElement('button');
                 newSpace.classList.add(row + ',' + column);
 
-                newSpace.onclick = function() {gameBoard.turnDecider(newSpace)};
+                newSpace.onclick = function() {gameBoard.makeMove(newSpace)};
 
                 //set DOM content to the board array content
                 if (boardArray[row][column]) {
@@ -61,12 +91,7 @@ const displayController = (() => {
 })();
 
 const playerCreater = (name, sign) => {
-    clickSpace = (button) => {
-        let x = button.classList[0][0];
-        let y = button.classList[0][2];
-        gameBoard.makeMove(x, y, sign);
-    }
-    return {name, sign, clickSpace};
+    return {name, sign};
 }
 
 const player1 = playerCreater('Reese', 'X');
